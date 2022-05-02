324bff5d4f0d719ece7e9a9a7851fe4dda9afb6a
spring98 <foreverspring98@gmail.com>
2022-05-02 12:51:27 +0900

    simple view

---
+++ test0502/view/view_controller/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  test0502
+//
+//  Created by spring on 2022/05/02.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController : UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
---
+++ test0502/view/view_controller/NavigationBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test0502
+//
+//  Created by spring on 2022/05/02.
+//
+
+import UIKit
+
+class NavigationBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // first commit test
+    }
+
+
+}
+
