e8712bf8ffed34c732adf092e1299e923d7a3810
spring98 <foreverspring98@gmail.com>
2022-05-02 12:17:48 +0900

    initial commit

---
+++ test0502UITests/test0502UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  test0502UITestsLaunchTests.swift
+//  test0502UITests
+//
+//  Created by spring on 2022/05/02.
+//
+
+import XCTest
+
+class test0502UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
