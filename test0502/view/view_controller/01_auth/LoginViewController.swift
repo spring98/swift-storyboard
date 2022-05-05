43a562996803206b6b39531dd33735290216cb44
spring98 <foreverspring98@gmail.com>
2022-05-05 14:02:42 +0900

    webview kakao map

--- test0502/view/view_controller/01_auth/LoginViewController.swift
+++ test0502/view/view_controller/01_auth/LoginViewController.swift
@@ -12,21 +12,18 @@ class LoginViewController : UIViewController {
     
     @IBOutlet weak var loginBtn: UIButton!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.addSubview(self.label)
-//        self.view.addSubview(self.controlSwitch)
-        self.view.addSubview(self.loginSignupSwitchBtn)
+        self.view.addSubview(self.onButton)
+        self.view.addSubview(self.offButton)
         self.view.addSubview(self.testBtn)
         
-        setSwitchBtnLayout()
+        setOnButtonLayout()
+        setOffButtonLayout()
         setTestBtnLayout()
     }
-    
-    
     
     lazy var label: UILabel = {
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
@@ -43,16 +40,39 @@ class LoginViewController : UIViewController {
         return label
     }()
     
-    lazy var loginSignupSwitchBtn:UIButton = {
+    lazy var onButton:UIButton = {
         let button :UIButton = UIButton()
-        button.setTitle("view change", for: .normal)
+        button.setTitle("view on", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .orange
+        button.addTarget(self, action: #selector(onButton(sender:)), for: .touchUpInside)
         
         return button
     }()
     
+    @objc func onButton(sender:UIButton) {
+        self.label.text = "On"
+        self.label.backgroundColor = UIColor.orange
+    }
     
+    
+    lazy var offButton:UIButton = {
+        let button :UIButton = UIButton()
+        button.setTitle("view off", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .orange
+        button.tag = 0
+        button.addTarget(self, action: #selector(offButton(sender:)), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    @objc func offButton(sender:UIButton) {
+        self.label.text = "Off"
+        self.label.backgroundColor = UIColor.gray
+    }
+    
+    // test button
     lazy var testBtn:UIButton = {
         let button :UIButton = UIButton()
         button.setTitle("test button", for: .normal)
@@ -63,52 +83,39 @@ class LoginViewController : UIViewController {
         return button
     }()
     
-    lazy var controlSwitch: UISwitch = {
-        let swicth: UISwitch = UISwitch()
-        swicth.layer.position = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height - 200)
-        swicth.tintColor = UIColor.orange
-        swicth.isOn = true
-        swicth.addTarget(self, action: #selector(onClickSwitch(sender:)), for:UIControl.Event.valueChanged)
-        
-        return swicth
-    }()
+    @objc func onClickTestButton(sender:UIButton) {
+        let tabBarSB = UIStoryboard(name: "TabBar", bundle: nil)
+        let tabBarController = tabBarSB.instantiateViewController(withIdentifier: "HomeTabBarController") as! HomeTabBarController
+        self.navigationController?.pushViewController(tabBarController, animated: true)
+    }
     
-    func setSwitchBtnLayout() {
-        loginSignupSwitchBtn.translatesAutoresizingMaskIntoConstraints = false
-        loginSignupSwitchBtn.topAnchor.constraint(equalTo: label.bottomAnchor,constant: 30).isActive = true
-        loginSignupSwitchBtn.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
-        loginSignupSwitchBtn.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+    
+    
+  
+    func setOnButtonLayout() {
+        onButton.translatesAutoresizingMaskIntoConstraints = false
+        onButton.topAnchor.constraint(equalTo: label.bottomAnchor,constant: 30).isActive = true
+        onButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        onButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+    }
+    
+    func setOffButtonLayout() {
+        offButton.translatesAutoresizingMaskIntoConstraints = false
+        offButton.topAnchor.constraint(equalTo: onButton.bottomAnchor,constant: 5).isActive = true
+        offButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        offButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
     }
     
     func setTestBtnLayout() {
         testBtn.translatesAutoresizingMaskIntoConstraints = false
-        testBtn.topAnchor.constraint(equalTo: loginBtn.bottomAnchor,constant: 30).isActive = true
+        testBtn.topAnchor.constraint(equalTo: offButton.bottomAnchor,constant: 30).isActive = true
         testBtn.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
         testBtn.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
     }
    
-    @objc func onClickTestButton(sender:UIButton) {
-        print("버튼 클릭")
-        
-        let tabBarSB = UIStoryboard(name: "TabBar", bundle: nil)
-        let tabBarController = tabBarSB.instantiateViewController(withIdentifier: "HomeTabBarController") as! HomeTabBarController
-        self.navigationController?.pushViewController(tabBarController, animated: true)
-
-    }
     
-    @objc func onClickSwitch(sender: UISwitch) {
-        var text: String!
-        var color: UIColor!
-        if sender.isOn {
-            text = "On"
-            color = UIColor.gray
-        } else {
-            text = "Off"
-            color = UIColor.orange
-        }
-        self.label.text = text
-        self.label.backgroundColor = color
-    }
+   
+  
 
     
 }
